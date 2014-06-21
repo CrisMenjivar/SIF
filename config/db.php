f483823113fbe9fa7318716d91ae062381dfe9d6
danielpunx1 <daniel_punx1@hotmail.com>
2014-06-20 18:16:23 -0600

    c

--- config/db.php
+++ config/db.php
@@ -1,4 +1,4 @@
 <?php
     $servidor="localhost"; $usuario="registroast"; $contra="registroast"; $nombre_bd="registroast";
-    $conexion=mysqli_connect($servidor, $usuario, $contra, $nombre_bd);
+    //$conexion=mysqli_connect($servidor, $usuario, $contra, $nombre_bd);
 ?>
